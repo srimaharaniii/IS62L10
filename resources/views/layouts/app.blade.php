7463ec68c9d29a9333845cb4484f1b6af56472b2
putrafahrezi <putrafahrezi89@gmail.com>
2025-05-16 10:09:47 +0700

    mmeperbaiki halaman

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,8 +30,16 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/mahasiswa">Data Mahasiswa</a>
+                        </li>
 
+                        <li class="nav-item">
+                            <a class="nav-link" href="/dosen">Data Dosen</a>
+                        </li>
                     </ul>
+
+
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\dosenController;
+use Illuminate\Support\Facades\Auth;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,67 +16,66 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/home', function () {
+    return view('welcome');
+});
 
 // Route::get('/cth', function () {
 //     return view('contoh');
 // });
 
-// # halaman untuk yang tidak di pakai
 // Route::fallback(function () {
 //     return view('notfound');
 // });
 
-// #halaman panggil nama
-// Route::get('mahasiswa/{nama}', function ($nama) {
-//     echo "Ini Halaman Masiswa dengan nama $nama";
-// });
+// // Route::get('mahasiswa/{nama}', function ($nama) {
+// //     echo "Ini Halaman Mahasiswa dengan Nama $nama";
+// // });
 
 // Route::get('/hello/hello2/hellolagi', function () {
-//     echo "Hello Word";
-// });
-
-// Route::get('mahasiswa',function (){
-//     $kelas = "IS63";
-//     $data = ["Herdio","Rani","Putri","Erik"];
-
-//with untuk mengubah nama
-// return view('mahasiswa.index')->with('mhs',$data) ->with ('kls',$kelas);
-
-//compact untuk memanggil langsung variabel nya
-//     return view ('mahasiswa.index',compact('kelas','data'));
+//     echo "Hello World";
 // });
 
 // Route::get('/mahasiswa', function () {
-//     $nama = "Sri Maharani";
-//     $nilai = 20;
+//     $kelas = "IS63";
+//     $data = ["Khalifah","Herdio","Badri","Raihan","Putri","Hanif"];
 
-//     $nilai2 = [40, 70, 35, 65, 100];
-
-//     return view('mahasiswa', compact('nama', 'nilai', 'nilai2'));
+//     // return view('mahasiswa.index')->with('mhs',$data)->with('kls',$kelas);
+//     return view('mahasiswa.index',compact('kelas','data'));
 // });
 
-Route::get('/master', function () {
-    $data_mhs = ["abdul","aidil","alif","asfal"];
-    return view('layout.master', compact('data_mhs'));
-});
+// Route::get('/mahasiswa', function () {
+//     $nama = "Herdio Saputra";
+//     $nilai = -40;
 
-Route::get('/mahasiswa', function () {
-    $data_mhs = ["abdul","aidil","alif","asfal"];
-    return view('data.mahasiswa', compact('data_mhs'));
-});
+//     $nilai2 = [85,70,50,70,35,100];
 
-Route::get('/dosen', function () {
-    $data_dos = ["ismanudin","mustofa","Dzulgunar","maulian"];
-    return view('data.dosen', compact('data_dos'));
-});
+//     return view('mahasiswa',compact('nama','nilai','nilai2'));
+// });
 
-Route::get('/galeri', function () {
-    return view('data.galeri');
-});
+// Route::get('/', function () {
+//     $data_mhs = ["Abdul","Adha","Aidil","Alif","Asfal"];
+//     return view('layout.master',compact('data_mhs'));
+// });
+
+// Route::get('/mahasiswa', function () {
+//     $data_mhs = ["Abdul","Adha","Aidil","Alif","Asfal"];
+//     return view('data.mahasiswa',compact('data_mhs'));
+// });
+
+// Route::get('/dosen', function () {
+//     $data_dos = ["Ismanuddin","Mustofa Lutfi","Rita Warni","Ridha Ansari","Dzulgunar M Nasir"];
+//     return view('data.dosen',compact('data_dos'));
+// });
+
+// Route::get('/galeri', function () {
+//     return view('data.galeri');
+// });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//Data Dosen
+Route::get('/dosen', [dosenController::class, 'index']);
+Route::get('/dosen/tambah', [dosenController::class, 'create']);
